54cbddb043a176cc568e9b93e222f64486559f34
alexjustdoit225 <alex.minnie.aw@gmail.com>
2022-03-06 17:16:14 -0600

    created dino block

--- script.js
+++ script.js
@@ -3,7 +3,32 @@ const canvas = document.getElementById('canvas1');
 const c = canvas.getContext('2d');
 
 const CANVAS_WIDTH = canvas.width = 800; 
-const CANVAS_HEIGHT = canvas.height = 600; 
+const CANVAS_HEIGHT = canvas.height = 500; 
 
+class Dino {
+    constructor() {
+        this.width = 20; 
+        this.height = 50; 
+        this.color = '#8338ec'; 
+        this.x = 0; 
+        this.y = canvas.height - this.height; 
+    }
 
+    update() {
 
+    }
+
+    draw() {
+        c.fillRect(this.x, this.y, this.width, this.height); 
+        c.fillStyle = this.color; 
+    }
+
+};
+
+const dino = new Dino(); 
+
+function animate() {
+    dino.draw();  
+    requestAnimationFrame(animate); 
+}
+animate(); 
