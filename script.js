8ff698c7d54c01f8f75ae26db3455de7d6c015ab
alexjustdoit225 <alex.minnie.aw@gmail.com>
2022-03-06 16:40:49 -0600

    setup script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+/** @type {HTMLCanvasElement} */
+const canvas = document.getElementById('canvas1'); 
+const c = canvas.getContext('2d');
+
+const CANVAS_WIDTH = canvas.width = 800; 
+const CANVAS_HEIGHT = canvas.height = 600; 
+
+
